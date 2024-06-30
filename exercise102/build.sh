e561c139a9ec815dc5fca637e1d03309d4e4726c
Mikko T <email@example.com>
2024-06-30 13:14:53 +0800

    add image tag from digest

--- exercise102/build.sh
+++ exercise102/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 docker build -t project:latest .
-k3d image import project:latest
+DIGEST=$(docker images --no-trunc --quiet project:latest | cut -d':' -f2)
+echo $DIGEST > digest.txt
+export DIGEST=$DIGEST
+docker image tag project:latest project:$DIGEST
+k3d image import project:$DIGEST
+sed -i "s/latest/$DIGEST/g" manifests/deployment.yaml
